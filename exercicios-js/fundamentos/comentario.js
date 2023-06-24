2c13550e266b22c56dbb8b70c29dbc258f017c58
RodrigoRamone7 <69323304+RodrigoRamone7@users.noreply.github.com>
2023-06-24 11:54:05 -0300

    progressão do curso

---
+++ exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,9 @@
+// comentario
+console.log('linha1')
+
+/*
+Vários comentários
+em várias linhas
+*/
+console.log('Linha2')
+
---
+++ exercicios-js/fundamentos/tipagemfraca.js
@@ -0,0 +1,7 @@
+let qlqr = 'Legal'
+console.log(qlqr)
+console.log(typeof qlqr)
+
+qlqr = 3.1516
+console.log(qlqr)
+console.log(typeof qlqr)
